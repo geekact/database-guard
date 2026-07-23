71338641d5f77a978000134100e2f6ce69eb6bb3
geekact <geekacting@gmail.com>
2026-07-23 20:16:07 +0800

    chore: 减少不必要的打包文件

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,15 +5,18 @@ export default defineConfig([
     entry: './src/backup-db.ts',
     outDir: './dist',
     format: 'esm',
+    dts: false,
   },
   {
     entry: './src/backup-binlog.ts',
     outDir: './dist',
     format: 'esm',
+    dts: false,
   },
   {
     entry: './src/restore-db.ts',
     outDir: './dist',
     format: 'esm',
+    dts: false,
   },
 ]);
